38248cd439987640984919d38969a46ccfcbe531
LukasMod <lukaszmod@o2.pl>
2020-02-01 14:21:45 +0100

    OrmLite - TableUtils - Columns

--- src/main/java/pl/ormlite/example/Book.java
+++ src/main/java/pl/ormlite/example/Book.java
@@ -1,8 +1,11 @@
 package pl.ormlite.example;
 
 
+import com.j256.ormlite.field.DataType;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
+
+import java.util.Date;
 
 @DatabaseTable(tableName = "books")
 public class Book {
@@ -11,14 +14,110 @@ public class Book {
     public Book() {
     }
 
-    //ID w tabeli (niepowtarzające się)
-    @DatabaseField(generatedId = true)
-    private int id;
+//    //ID w tabeli (niepowtarzające się)
+//    @DatabaseField(generatedId = true)
+//    private int id;
 
-    @DatabaseField(columnName = "TITLE")
+    @DatabaseField(columnName = "TITLE", canBeNull = false) //nigdy nie może być nullem
     private String title;
+
+    @DatabaseField(columnName = "DESCRIPTION", dataType = DataType.LONG_STRING) //dla Stringów powyżej 256 znaków
+    private String description;
+
+    @DatabaseField(columnName = "ISBN", unique = true) //zawsze unikalne elementy
+    private String isbn;
+
+    @DatabaseField(columnName = "ADDED_DATE")
+    private Date addedDate;
+
+    @DatabaseField(columnName = "DATE_RELEASE", dataType = DataType.DATE_STRING, format = "yyyy-MM-DD")
+    private Date dateRelease;
+
+    @DatabaseField(columnName = "RATING",width = 1) //maksymalna ilość znaków (nie działa np. na sqlite, ale na h2 działa)
+    private String rating;
+
+    @DatabaseField(columnName = "BORROWED", defaultValue = "false") //domyślna wartość
+    private boolean borrowed;
 
     @DatabaseField(columnName = "PRICE")
     private double price;
 
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Date getAddedDate() {
+        return addedDate;
+    }
+
+    public void setAddedDate(Date addedDate) {
+        this.addedDate = addedDate;
+    }
+
+    public Date getDateRelease() {
+        return dateRelease;
+    }
+
+    public void setDateRelease(Date dateRelease) {
+        this.dateRelease = dateRelease;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public boolean isBorrowed() {
+        return borrowed;
+    }
+
+    public void setBorrowed(boolean borrowed) {
+        this.borrowed = borrowed;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", addedDate=" + addedDate +
+                ", dateRelease=" + dateRelease +
+                ", rating='" + rating + '\'' +
+                ", borrowed=" + borrowed +
+                ", price=" + price +
+                '}';
+    }
 }
--- src/main/java/pl/ormlite/example/Main.java
+++ src/main/java/pl/ormlite/example/Main.java
@@ -7,19 +7,22 @@ import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
 
 import java.io.IOException;
+
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
 
 
-    public static void main(String[] args) throws SQLException, IOException {
+    public static void main(String[] args) throws SQLException, IOException, ParseException {
 
         // this uses h2 but you can change it to match your database
         // String databaseUrl = "jdbc:h2:mem:account";
 
 
-
-       // String databaseUrlH2 = "jdbc:H2:./database";  //ścieżka do bazy danych H2 w katalogu głównym
+         String databaseUrlH2 = "jdbc:h2:./database";  //ścieżka do bazy danych H2 w katalogu głównym
         //Baza H2 wymaga dodania do POM dependency nowych
         // ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrlH2);
 //
@@ -32,7 +35,7 @@ public class Main {
 //        accountDao.create(account);
 
         String databaseUrl = "jdbc:sqlite:database.db";  //ścieżka do bazy danych w katalogu głównym
-        ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrl);
+        ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrlH2);
 
 
         //najpierw usuwa tabele, pozniej tworzy nowe. 'true' na koncu pomija bledy
@@ -40,8 +43,25 @@ public class Main {
         TableUtils.dropTable(connectionSource, Book.class, true);
         //chroni przed nadpisaniem tablicy, chroni przed wyjatkiem jesli tabela istnieje
         TableUtils.createTableIfNotExists(connectionSource, Book.class);
-      //  TableUtils.createTable(connectionSource, Book.class);
+        //  TableUtils.createTable(connectionSource, Book.class);
 
+        Book book = new Book();
+        book.setTitle("Władca Pierścieni");
+        book.setIsbn("11111");
+        book.setAddedDate(new Date());
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+        String dateInString = "2012/11/11";
+        Date date = sdf.parse(dateInString);
+
+        book.setDateRelease(date);
+        book.setRating("1");
+        book.setBorrowed(true);
+        book.setPrice(33.99);
+
+        Dao<Book, ?> dao = DaoManager.createDao(connectionSource, Book.class);
+
+        dao.create(book);
         connectionSource.close();  //zamyka połączenie z bazą danych, odciąża pamięć
     }
 
